2aec3bf38cb505bbe1b73128934c4b0f03c036a0
estradakev <kevinestrada530@gmail.com>
2021-06-18 23:32:08 -0700

    first blog tutorial

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/kevinestrada/code_space/websites/Mock Ups/djangogirls/myvenv/bin/python3.9
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
